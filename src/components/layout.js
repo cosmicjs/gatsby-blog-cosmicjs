fc573045c6ad5225fa691e14c3f38273ae798f19
Jazib Sawar <jazibsawar@gmail.com>
2018-10-03 23:07:27 +0500

    Updated to gatsby v2

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Link from 'gatsby-link'
+import { Link } from 'gatsby'
 import { StaticQuery, graphql } from 'gatsby'
 
 import cosmicjsLogo from '../../static/cosmicjs.svg'
