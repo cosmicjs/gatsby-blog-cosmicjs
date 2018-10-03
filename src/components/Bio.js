d5e36c6183f762857a885c5f10518323f5e127b4
Jazib Sawar <jazibsawar@gmail.com>
2018-10-03 20:09:22 +0500

    Updated to gatsby v2

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,36 +1,23 @@
 import React from 'react'
-
-// Import typefaces
-import 'typeface-montserrat'
-import 'typeface-merriweather'
-
-import avatar from './avatar.png'
 import { rhythm } from '../utils/typography'
 
-class Bio extends React.Component {
-  render() {
-    const settings = this.props.settings
-    return (
-      <div
-        style={{
-          display: 'flex',
-          marginBottom: rhythm(2.5),
-        }}
-      >
-        <img
-          src={settings.author_avatar.imgix_url}
-          alt={settings.author_name}
-          style={{
-            marginRight: rhythm(1 / 2),
-            marginBottom: 0,
-            width: rhythm(2),
-            height: rhythm(2),
-          }}
-        />
-        <div dangerouslySetInnerHTML={{ __html: settings.author_bio }} />
-      </div>
-    )
-  }
-}
-
-export default Bio
+export default ({ settings }) => (
+  <div
+    style={{
+      display: 'flex',
+      marginBottom: rhythm(2.5),
+    }}
+  >
+    <img
+      src={settings.author_avatar.imgix_url}
+      alt={settings.author_name}
+      style={{
+        marginRight: rhythm(1 / 2),
+        marginBottom: 0,
+        width: rhythm(2),
+        height: rhythm(2),
+      }}
+    />
+    <div dangerouslySetInnerHTML={{ __html: settings.author_bio }} />
+  </div>
+)
