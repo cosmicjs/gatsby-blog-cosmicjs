ddcd9c02cf449d0829435263f8223c35ea197ab7
jazibsawar <jazibsawar@gmail.com>
2020-04-20 17:20:11 +0300

    FIXED: React Helmet import issue after update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import get from 'lodash/get'
-import Helmet from 'react-helmet'
+import { Helmet } from 'react-helmet'
 import { graphql } from 'gatsby'
 
 import Bio from '../components/Bio'
