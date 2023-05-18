ff533d95dd72f71ddaad4c7b51d617681f36845e
Sagar Kumar Shrivastava <sinhasagar01@gmail.com>
2023-05-18 22:04:04 +0530

    Update Bio.js

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -16,6 +16,7 @@ export default ({ settings }) => (
         marginBottom: 0,
         width: rhythm(2),
         height: rhythm(2),
+        borderRadius: 35px,
       }}
     />
     <div dangerouslySetInnerHTML={{ __html: settings.author_bio }} />
